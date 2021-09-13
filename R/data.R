586b284faf3feb3d255ba6f1389deb4821d0037d
Andrija Djurovic <85941317+andrija-djurovic@users.noreply.github.com>
2021-09-13 20:37:03 +0200

    Delete R directory

--- R/data.R
+++
@@ -1,9 +0,0 @@
-#' German Credit Data
-#'
-#' The German Credit Data contains data on 20 variables and the classification whether 
-#' an applicant is considered a Good or a Bad credit risk for 1000 loan applicants.
-#' Name of the columns are used as give in the source file. 
-#' Note that subset of those data is available also in 'monobin' package (gcd) and used for some examples in 'PDtoolkit' package.
-#'  
-#' @source \url{https://online.stat.psu.edu/stat857/node/215/}
-"loans"
