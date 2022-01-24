f437354603fdc9d52d7cb68d0728bf558a3fc674
Andrija Djurovic <85941317+andrija-djurovic@users.noreply.github.com>
2022-01-24 10:05:39 +0100

    Add files via upload

--- R/00_GLOBALS.R
+++ R/00_GLOBALS.R
@@ -2,4 +2,5 @@
 utils::globalVariables(c(".", "bin.type", "complete.cases", "dist.b", "dist.g", "glm", "iv.b", "nb", "ng", "no",
 			 "pct", "predict", "sb", "sg", "woe", "combn", "cor", "cutree", "dist", "group", "label",
 			 "label.y", "pchisq", "pnorm", "xtabs", "as.dist", "reg.tree", "segment", "mpred",
-			 "nb.obs", "nb.mod", "dr.mod", "dr.obs", "amse", "as.formula", "auc", "db.vld", "rmse"))
+			 "nb.obs", "nb.mod", "dr.mod", "dr.obs", "amse", "as.formula", "auc", "db.vld", "rmse", "bin",
+			 "n.base", "n.target"))
