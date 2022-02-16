f84f8fd6ee587db759874637bf2d06ba640f5336
Andrija Djurovic <85941317+andrija-djurovic@users.noreply.github.com>
2022-02-16 09:43:29 +0100

    Add files via upload

---
+++ R/17_EVRS.R
@@ -0,0 +1,219 @@
+#' Modelling the Economic Value of Credit Rating System
+#'
+#' \code{evrs} calculates the economic benefits of improved PD model based on increase of portfolio return.
+#' Implemented algorithm replicates the framework presented in the Reference under assumption that 
+#' bank adopts continuous PD rating scale. Despite this assumption, results are almost identical for scenarios
+#' of base case portfolio from the Reference.
+#'@param db Data frame with at least the following columns: default indicator (target), PDs of model in use, 
+#'		PDs of benchmark model and LGD values.
+#'@param pd Name of PD of model in use within db argument.
+#'@param benchmark Name of PD of benchmark model within db argument.
+#'@param target Name of target (default indicator 0/1) within db argument.
+#'@param sigma Measurement error of model in use. If default value (\code{NA}) is passed, then measurement error
+#'		   is calculated as standard deviation of PD difference of model in use and benchamark model.
+#'@param r Risk-free rate.
+#'@param elasticity Elasticity parameter used to define customer churn in case of loan overpricing. 
+#'@param prob.to.leave.threshold Threshold for customers' probability to leave in case of loan overpricing. 
+#'@param sim.num Number of simulations. Default is 500. 
+#'@param seed Random seed to ensure reproducibility. Default is 991. 
+#'@return The command \code{evrs} returns a list of two elements. The first element is data frame 
+#' \code{summary.tbl} and it provides simulation summary: number of simulations, number of successful simulations,
+#' population size (number of observations of supplied \code{db} data frame), measurment error, 
+#' average churn value (number of customers that left the portfolio due to the overpricing), average return of simulated 
+#' portfolios, return of benchmark portfolio and retrun difference (main result of the simulation). The second element is
+#' numeric vector of return averages of simulated portfolios.
+#'@references 
+#' Jankowitsch at al. (2007). Modelling the economic value of credit rating systems.
+#'	Journal of Banking & Finance, Volume 31, Issue 1, \doi{10.1016/j.jbankfin.2006.01.003}. 
+#'@examples
+#'@importFrom stats rnorm
+#'suppressMessages(library(PDtoolkit))
+#'data(loans)
+#'#simulate model in use
+#'#discretized numeric risk factors using ndr.bin from monobin package
+#'num.rf <- sapply(loans, is.numeric)
+#'num.rf <- names(num.rf)[!names(num.rf)%in%"Creditability" & num.rf]
+#'loans[, num.rf] <- sapply(num.rf, function(x) 
+#'				  ndr.bin(x = loans[, x], y = loans[, "Creditability"])[[2]])
+#'#bin all risk factors on max number of groups, pct of observations and defaults
+#'cat.rf <- names(loans)[!names(loans)%in%"Creditability"]
+#'loans[, cat.rf] <- sapply(cat.rf, function(x) 
+#'               			    cat.bin(x = loans[, x], 
+#'                   		    y = loans[, "Creditability"],
+#'                     		    sc = c("SC", NA),
+#'                    		    sc.merge = "closest",
+#'                     		    min.pct.obs = 0.05, 
+#'                     		    min.avg.rate = 0.01,
+#'                     		    max.groups = 5, 
+#'                     		    force.trend = "dr")[[2]])
+#'loans.woe <- replace.woe(db = loans, target = "Creditability")[[1]]
+#'#estimate model
+#'miu.formula <- Creditability ~ `Account Balance` +`Age (years)` + `Duration of Credit (month)` +
+#'		   			  `Value Savings/Stocks` + `Purpose`
+#'miu <- glm(miu.formula, family = "binomial", data = loans.woe)
+#'miu
+#'miu.pd <- unname(predict(miu, type = "response", newdata = loans.woe))
+#'
+#'#simulate benchmark model with interaction.transformer support
+#'data(loans)
+#'#define risk factors that will be used for interactions
+#'rf.mat <- matrix(c(1, 2, 14, 2, 6, 7, 4, 5, 19, 1, 3, 14, 7, 8, 12,
+#'			 8, 13, 16, 4, 13, 14, 8, 11, 17, 6, 9, 11,
+#'			 4, 10, 11), nrow = 3, byrow = FALSE) 
+#'rf.l <- ncol(rf.mat)
+#'it <- data.frame(matrix(NA, ncol = rf.l, nrow = nrow(loans)))
+#'it <- cbind.data.frame(Creditability = loans$Creditability, it)
+#'names(it)[-1] <- paste0("rf.inter", apply(rf.mat, 2, paste, collapse = "."))
+#'for	(i in 1:rf.l) {
+#'	it.l <- interaction.transformer(db = loans, 
+#'						  rf = names(loans)[rf.mat[, i] + 1], 
+#'				 		  target = "Creditability",
+#'						  min.pct.obs = 0.05,
+#'				 		  min.avg.rate = 0.01,
+#'				 		  max.depth = 3,
+#'				 		  monotonicity = TRUE,
+#'				 		  create.interaction.rf = TRUE)[[2]]
+#'	it[, i + 1] <- as.character(unname(c(it.l, recursive = TRUE)))
+#'	}
+#'it.woe <- replace.woe(db = it, target = "Creditability")[[1]]
+#'bnm <- glm(Creditability ~ ., family = "binomial", data = it.woe)
+#'bnm
+#'bnm.pd <- unname(predict(bnm, type = "response", newdata = it.woe))
+#'
+#'#prepare data for evrs function
+#'db <- data.frame("Creditability" = loans$Creditability, pd = miu.pd, pd.benchmark = bnm.pd, lgd = 0.75)
+#'#calculate the difference in portfolio return between model in use the benchmark model
+#'res <- evrs(db = db, 
+#'		pd = "pd", 
+#'		benchmark = "pd.benchmark", 
+#'		lgd = "lgd",
+#'		target = "Creditability",
+#'		sigma = NA, 
+#'		r = 0.03, 
+#'		elasticity = 100, 
+#'		prob.to.leave.threshold = 0.5,
+#'		sim.num = 500, 
+#'		seed = 991)
+#'names(res)
+#'#print simulation summary table
+#'res[["summary.tbl"]]
+#'#portfolio return increase in case of using benchmark model
+#'res[["summary.tbl"]][, "return.difference", drop = FALSE]
+#'res[["summary.tbl"]]$return.difference
+#'#summary of simulated returns
+#'summary(res[["return.sim"]])
+#'@export
+evrs <- function(db, pd, benchmark, lgd, target, sigma = NA, r, elasticity, prob.to.leave.threshold, 
+		     sim.num = 500, seed = 991) {
+	if	(!is.data.frame(db)) {
+		stop("db is not a data frame.")
+		}
+	cols.comp <- c(pd, benchmark, lgd, target)
+	if	(!sum(cols.comp%in%names(db)) == length(cols.comp)) {
+		miss.cols <- cols.comp[!cols.comp%in%names(db)]
+		msg <- paste0("Following column(s) does not exist in supplied db:",
+				  paste(miss.cols, collapse = ", "), ".")
+		stop(msg)
+		}	
+	db.sim <- db[, c(target, pd, benchmark, lgd)]
+	names(db.sim) <- c("target", "pd", "benchmark", "lgd")
+	db.sim <- db.sim[complete.cases(db.sim), ]
+	if	(!all(db.sim$target%in%c(0, 1))) {
+		stop("target is not 0/1 variable")
+		}
+	if	(!all(db.sim$pd > 0 & db.sim$pd < 1) |
+		 !all(db.sim$bechmark > 0 & db.sim$bechmark < 1)) {
+		stop("pd and bechmark has to be greater than 0 and less than 1.")
+		}
+	prob.to.leave.threshold <- prob.to.leave.threshold[1]
+	if	(!(prob.to.leave.threshold[1] > 0 & prob.to.leave.threshold[1] < 1)) {
+		stop("prob.to.leave.threshold has to be greater than 0 and less than 1.")
+		}
+	r <- r[1]; elasticity <- elasticity[1]; seed <- seed[1]
+	if	(!(r > 0 & elasticity > 0 & seed > 0)) {
+		stop("r, elasticity and seed have to be greater than 0.")
+		}
+	#measurment error
+	if	(is.na(sigma)) {
+		db.sim$pd.dif <- db.sim$pd - db.sim$benchmark
+		sigma <- sd(db.sim$pd.dif)
+		if	(sigma == 0) {
+			stop("sigma cannot be calculated. Check pd.benchmark values.")
+			}
+		} else {
+		if	(!is.numeric(sigma) | length(sigma) > 1) {
+			stop("sigma has to be single numeric value greater than 0.")} 
+		if	(sigma <= 0) {stop("sigma has to be single numeric value greater than 0.")}		
+		}
+	#calculate existing spreads
+	db.sim$spread.exis <- spread(r = r, pd = db.sim$pd, lgd = db.sim$lgd)
+	#simulations
+	churn <- rep(NA, sim.num)
+	r.sim <- rep(NA, sim.num)
+	check.counter <- 0
+	for	(i in 1:sim.num) {
+		pd.sim <- pd.observed(pd = db.sim$pd, mean = 0, sigma = sigma, seed = seed + (i - 1))
+		spread.sim <- spread(r = r, pd = pd.sim, lgd = db.sim$lgd)
+		magnitude <- spread.sim - db.sim$spread.exis
+		probs <- prob.to.leave(alpha = elasticity, 
+					     magnitude = ifelse(magnitude < 0, 0, magnitude))
+		sim.indx <- !probs > prob.to.leave.threshold
+		churn.l <- sum(probs > prob.to.leave.threshold)
+		churn[i] <- churn.l
+		db.sim.l <- db.sim[sim.indx, ]
+		r.sim[i] <- portfolio.return(def.ind = db.sim.l$target, 
+						     r = r, 
+						     spread = spread.sim[sim.indx], 
+						     lgd = db.sim.l$lgd)
+		}
+	return.benchmark <- portfolio.return(def.ind = db.sim$target, 
+					     r = r, 
+					     spread = spread(r = r, 
+							     pd = db.sim$benchmark, 
+							     lgd = db.sim$lgd), 
+					     lgd = db.sim$lgd)
+
+	summary.tbl <- data.frame(sim.total = sim.num,
+					  sim.succes = sum(!is.na(r.sim)),
+					  population = nrow(db.sim), 
+					  measurment.error = sigma,
+					  churn.avg = mean(churn, na.rm = TRUE), 
+					  return.sim = mean(r.sim, na.rm = TRUE),
+					  return.benchmark = return.benchmark)
+	summary.tbl$return.difference <- summary.tbl$return.benchmark - summary.tbl$return.sim	
+	res <- list(summary.tbl = summary.tbl, return.sim = r.sim)
+return(res)
+}
+
+score.trans <- function(x, type) {
+	if	(type%in%"pd.to.score") {
+		res <- log(x / (1 - x))
+		} else {
+		res <- exp(x) / ( 1 + exp(x))
+		}
+return(res)
+}
+pd.observed <- function(pd, mean = 0, sigma, seed) {
+	set.seed(seed)
+	score <- score.trans(pd, type = "pd.to.score")
+	nobs <- length(pd)
+	score.sim <- score + rnorm(nobs, mean = mean, sd = sigma)
+	pd.sim <- score.trans(score.sim, type = "score.to.pd")
+return(pd.sim)
+}
+spread <- function(r, pd, lgd) {
+	res <- (1 + r) * (pd * lgd) / (1 - pd * lgd)
+return(res)
+}
+prob.to.leave <- function(alpha, magnitude) {
+	res <- 1 - exp(-alpha * magnitude) 
+return(res)
+} 
+portfolio.return <- function(def.ind, r, spread, lgd) {
+	ret <- ifelse(def.ind == 0, 1 + r + spread - 1, (1 - lgd) * (1 + r + spread) - 1)	
+	ret.mean <- mean(ret)
+return(ret.mean)
+}
+
+
+
---
+++ R/18_INTERACTION_TRANSFORMER.R
@@ -0,0 +1,331 @@
+#' Extract risk factors interaction from decision tree
+#'
+#' \code{interaction.transformer} extracts the interaction between supplied risk factors from decision tree.
+#' It implements customized decision tree algorithm that takes into account different conditions such as minimum 
+#' percentage of observations and defaults in each node, maximum tree depth and monotonicity condition 
+#' at each splitting node. Gini index is used as metric for node splitting .
+#'@param db Data frame of risk factors and target variable supplied for interaction extraction.
+#'@param rf Character vector of risk factor names on which decision tree is run.
+#'@param target Name of target variable (default indicator 0/1) within db argument.
+#'@param min.pct.obs Minimum percentage of observation in each leaf.
+#'@param min.avg.rate Minimum percentage of defaults in each leaf.
+#'@param max.depth Maximum number of splits.
+#'@param monotonicity Logical indicator. If \code{TRUE}, observed trend between risk factor and target will be preserved
+#'			    in splitting node.
+#'@param create.interaction.rf Logical indicator. If \code{TRUE}, second element of the output will be data frame with
+#'					 interaction modalities.
+#'@return The command \code{interaction.transformer} returns a list of two data frames. The first data frame provides
+#' the tree summary. The second data frame is a new risk factor extracted from decision tree.
+#'@examples
+#'@import dplyr
+#'suppressMessages(library(PDtoolkit))
+#'data(loans)
+#'modify risk factors in order to show how the function works with missing values
+#'loans$"Account Balance"[1:10] <- NA
+#'loans$"Duration of Credit (month)"[c(13, 15)] <- NA
+#'it <- interaction.transformer(db = loans, #
+#'					 rf = c("Account Balance", "Duration of Credit (month)"), 
+#'					 target = "Creditability",
+#'					 min.pct.obs = 0.05,
+#'					 min.avg.rate = 0.01,
+#'					 max.depth = 2,
+#'				 	 monotonicity = TRUE,
+#'					 create.interaction.rf = TRUE)
+#'names(it)
+#'it[["tree.info"]]
+#'tail(it[["interaction"]])
+#'table(it[["interaction"]][, "rf.inter"], useNA = "always")
+#'@export
+interaction.transformer <- function(db, rf, target, min.pct.obs, min.avg.rate, max.depth, monotonicity, create.interaction.rf) {
+	if	(!is.data.frame(db)) {
+		stop("db is not a data frame.")
+		}
+	if	(any(!c(rf, target)%in%names(db))) {
+		stop("rf or target variable do not exist in supplied db.")
+		}
+	if	(!is.numeric(min.pct.obs) | !is.numeric(min.avg.rate) |
+		 min.pct.obs <= 0 | min.pct.obs >= 1 |
+		 min.avg.rate <= 0 | min.avg.rate >= 1) {
+		stop(" min.pct.obs and min.avg.rate has to be numeric values greater than 0 and less than 1")
+		}
+	if	(!is.na(max.depth)) {
+		if	(!is.numeric(max.depth) | max.depth < 2) {
+			stop("max.depth has to be numeric value greater than 2.")
+			}
+		}
+	if	(is.na(monotonicity) | !is.logical(monotonicity)) {
+		stop("monotonicity has to be logical value - TRUE or FALSE")
+		}
+	if	(is.na(create.interaction.rf) | !is.logical(create.interaction.rf)) {
+		stop("create.interaction.rf has to be logical value - TRUE or FALSE")
+		}	
+	d <- db[, c(target, rf)]
+	d <- d[!is.na(d[, target]), ]
+	nr <- nrow(d)
+	if	(nr == 0) {
+		stop("target contains only missing values.")
+		}
+	if	(length(unique(d[, target])) == 1) {
+		stop("target has only 1 class.")
+		}
+	if	(!sum(d[, target]%in%c(0, 1)) == nr) {
+		stop("target contains value(s) different than 0 and 1.")
+		}
+	nd <- sum(d[, target])
+	rf.check <- sapply(d[, rf, drop = FALSE], function(x) sum(is.na(x)) == length(x))
+	if	(any(rf.check)) {
+		msg <- paste0("The following risk factor(s) contain(s) only missing values: ", 
+				  paste0(names(rf.check)[rf.check], collapse = ", "), ".")
+		stop(msg)
+		}		
+	tree.res <- dt.algo(d = d, 
+			    rf = rf, 
+			    target = target, 
+			    min.obs = ceiling(ifelse(nr * min.pct.obs < 30, 30, nr * min.pct.obs)), 
+			    min.rate = ceiling(ifelse(nd * min.avg.rate < 1, 1, nd * min.avg.rate)), 
+			    max.depth = max.depth, 
+			    monotonicity = monotonicity)
+	if	(create.interaction.rf) {
+		interaction <- extract.interactions(db = db, tree.info = tree.res)
+		} else {
+		interaction <- data.frame()
+		}
+return(list(tree.info = tree.res, interaction = interaction))
+}
+
+#decision tree algo
+dt.algo <- function(d, rf, target, min.obs, min.rate, max.depth, monotonicity) {
+	rf.num <- sapply(d[, rf, drop = FALSE], is.numeric)
+	rf.num <- names(rf.num)[rf.num]
+	rf.cat <- rf[!rf%in%rf.num]	
+	if	(monotonicity & length(rf.num) > 0) {
+		corr.dir <- sapply(rf.num, function(x) sign(cor(d[, x], d[, target], 
+						    			  method = "spearman", 
+									  use = "complete.obs")))
+		} else {
+		corr.dir <- c()
+		}
+	#initialize the process
+	do.splits <- TRUE
+	tree.info <- data.frame(node = 1, 
+				rf = NA, 
+				nobs = nrow(d), 
+				filter = NA,
+				terminal = "split",
+				stringsAsFactors = FALSE)
+	#start splitting
+	while	(do.splits) {
+		to.calculate <- which(tree.info$terminal == "split")
+		for	(i in to.calculate) {
+      		if	(!is.na(tree.info$filter[i])) {
+        			tbl.l <- subset(d, eval(parse(text = tree.info$filter[i])))
+      			} else {
+        			tbl.l <- d
+      			}	
+			splitting <- lapply(rf, function(x) {
+							best.split(tbl = tbl.l, 
+								     rf.l = x, 
+								     target = target, 
+								     min.obs = min.obs, 
+								     min.rate = min.rate,   
+								     monodir = corr.dir[x])})
+			bs.rf.indx <- which.max(sapply(splitting, "[[", "ssv"))
+			if	(length(bs.rf.indx) == 0) {
+				tree.info$terminal[i] <- "leaf"
+				next
+				}
+			tmp.sp <- splitting[[bs.rf.indx]]
+			mn <- max(tree.info$node) 
+			if	(tmp.sp$rf.t%in%"numeric") {
+				tmp.filter <- c(paste(tmp.sp$rf, "<", tmp.sp$split), paste(tmp.sp$rf, ">=", tmp.sp$split))
+				} else {
+				tmp.filter <- c(paste0(tmp.sp$rf, "%in%", tmp.sp$split), paste0("!", tmp.sp$rf, "%in%", tmp.sp$split))
+				}
+			if	(!is.na(tree.info$filter[i])) {
+				tmp.filter <- paste(tree.info$filter[i], tmp.filter, sep = " & ")
+			 	} 
+			tmp.filter.nobs <- paste(paste0("!is.na(", tmp.sp$rf, ")"), tmp.filter, sep = " & ")
+			tmp.nobs <- sapply(tmp.filter.nobs, FUN = function(i, x) {		
+									 nrow(subset(x = x, subset = eval(parse(text = i))))
+									 }, x = tbl.l) 
+			children <- data.frame(node = c(mn + 1, mn + 2),
+					       rf = tmp.sp$rf, 
+					       nobs = unname(tmp.nobs),
+				               filter = tmp.filter,
+					       terminal = rep("split", 2),
+					       row.names = NULL)    
+			tree.info$terminal[i] <- "parent"
+			tree.info <- rbind(tree.info, children)
+			if	(!is.na(max.depth) & sum(tree.info$terminal%in%c("leaf", "split")) > max.depth) {
+				tree.info$terminal[tree.info$terminal%in%"split"] <- "leaf"
+				do.splits <- FALSE
+				break
+				} 
+			}
+		do.splits <- !all(tree.info$terminal != "split")
+		}
+	tree.info$ind <- as.numeric(ave(tree.info$terminal, tree.info$terminal, 
+				   		  FUN = function(x) if	(all(x%in%"leaf")) {1:length(x)} else {NA}))
+return(tree.info)
+}
+#node split functions
+best.split <- function(tbl, rf.l, target, min.obs, min.rate, monodir) {
+	rf.c <- is.numeric(tbl[, rf.l])
+	if	(rf.c) {
+		 bs <- best.split.num(tbl = tbl, 
+				      rf.l = rf.l, 
+			  	      target = target, 
+				      min.obs = min.obs, 
+				      min.rate = min.rate,   
+				      monodir = monodir)
+		bs <- cbind.data.frame(rf.t = "numeric", bs)	
+		} else {
+		bs <- best.split.cat(tbl = tbl, 
+				     rf.l = rf.l, 
+				     target = target, 
+				     min.obs = min.obs, 
+				     min.rate = min.rate)
+		bs <- cbind.data.frame(rf.t = "categorical", bs)
+		}
+return(bs)
+}
+best.split.num <- function(tbl, rf.l, target, min.obs, min.rate, monodir) {
+	g <- unname(quantile(tbl[, rf.l],seq(1 / 50, 1, length.out = 50), type = 3, na.rm = TRUE))
+	g <- unique(c(min(tbl[, rf.l], na.rm = TRUE), g))
+	if	(length(g) == 1 | all(is.na(tbl[, rf.l]))) {
+		res <- data.frame(rf = paste0("`", rf.l, "`"), ssv = NA, split = NA, y.l = NA, y.r = NA)
+		return(res)
+		}
+    	tbl$bin <- cut(tbl[, rf.l], breaks = g, include.lowest = TRUE)
+	tbl <- tbl[complete.cases(tbl), ]
+	tbl.s <- tbl %>%
+		   group_by(bin) %>%
+		   summarise(n = n(),
+		   y.sum = sum(!!sym(target)),
+		   y.avg = mean(!!sym(target)), 
+		   x.min = min(!!sym(rf.l)),
+		   x.max = max(!!sym(rf.l))) %>%
+		   ungroup() %>%
+		   mutate(n.cs = cumsum(n),
+			    y.cs = cumsum(y.sum),
+			    y.cs.a = y.cs / n.cs,
+			    n.cs.rev = cumsum(rev(n)),
+			    y.cs.rev = cumsum(rev(y.sum)),
+			    y.cs.a.rev = y.cs.rev / n.cs.rev,
+			    dr.c = y.cs / n.cs,
+			    dr.r = (sum(y.sum) - y.cs) / (sum(n) - n.cs))
+	lt <- min(which(tbl.s$n.cs >= min.obs & tbl.s$y.cs >= min.rate)) + 1
+	ut <- nrow(tbl.s) - min(which(tbl.s$n.cs.rev >= min.obs & tbl.s$y.cs.rev >= min.rate)) 
+	if	(lt > ut) {
+		res <- data.frame(rf = paste0("`", rf.l, "`"), ssv = NA, split = NA, y.l = NA, y.r = NA)
+		return(res)
+		}
+	if	(length(monodir) > 0) {
+		if	(monodir == 1) {
+			ms <- which(tbl.s$dr.c < tbl.s$dr.r)
+			} else {
+			ms <- which(tbl.s$dr.c > tbl.s$dr.r)
+			}
+		} else {
+		ms <- lt:ut
+		}
+	sp <- intersect(lt:ut, ms)	
+	if	(length(sp) == 0) {
+		res <- data.frame(rf = paste0("`", rf.l, "`"), ssv = NA, split = NA, y.l = NA, y.r = NA)
+		return(res)
+		}
+	splits <- tbl.s$x.min[sp]
+	sl <- length(splits)
+	ssv <- rep(NA, sl)
+	for	(i in 1:sl) {
+		split.l <- splits[i]
+		ssv[i] <- gini.num(y = tbl[, target], x = tbl[, rf.l], sp = split.l)
+		}
+	split.at <- splits[which.max(ssv)]
+	y.l <- mean(tbl[tbl[, rf.l] < split.at, target])
+	y.r <- mean(tbl[tbl[, rf.l] >= split.at, target])
+	res <- data.frame(rf = paste0("`", rf.l, "`"), ssv = min(ssv), split = split.at, y.l = y.l, y.r = y.r)
+return(res)
+}
+best.split.cat <- function(tbl, rf.l, target, min.obs, min.rate) {
+	if	(all(is.na(tbl[, rf.l]))) {
+		res <- data.frame(rf = paste0("`", rf.l, "`"), ssv = NA, split = NA, y.l = NA, y.r = NA)
+		return(res)
+		}
+    	tbl$bin <- tbl[, rf.l]
+	tbl <- tbl[complete.cases(tbl), ]
+	tbl.s <- tbl %>%
+		   group_by(bin) %>%
+		   summarise(n = n(),
+		   y.sum = sum(!!sym(target)),
+		   y.avg = mean(!!sym(target)), 
+		   x.min = min(!!sym(rf.l)),
+		   x.max = max(!!sym(rf.l))) %>%
+		   ungroup() %>%
+		   mutate(n.cs = cumsum(n),
+			    y.cs = cumsum(y.sum),
+			    y.cs.a = y.cs / n.cs,
+			    n.cs.rev = cumsum(rev(n)),
+			    y.cs.rev = cumsum(rev(y.sum)),
+			    y.cs.a.rev = y.cs.rev / n.cs.rev,
+			    dr.c = y.cs / n.cs,
+			    dr.r = (sum(y.sum) - y.cs) / (sum(n) - n.cs))
+	lt <- min(which(tbl.s$n.cs >= min.obs & tbl.s$y.cs >= min.rate))
+	ut <- nrow(tbl.s) - min(which(tbl.s$n.cs.rev >= min.obs & tbl.s$y.cs.rev >= min.rate)) 
+	if	(lt > ut) {
+		res <- data.frame(rf = paste0("`", rf.l, "`"), ssv = NA, split = NA, y.l = NA, y.r = NA)
+		return(res)
+		}
+	splits <- lt:ut	
+	sl <- length(splits)
+	ssv <- rep(NA, sl)
+	for	(i in 1:sl) {
+		split.l <- tbl.s$bin[1:splits[i]]
+		ssv[i] <- gini.cat(y = tbl[, target], x = tbl[, rf.l], sp = split.l)
+		}
+	split.at <- splits[which.max(ssv)]
+	y.l <- mean(tbl[tbl[, rf.l]%in%tbl.s$bin[1:split.at], target])
+	y.r <- mean(tbl[!tbl[, rf.l]%in%tbl.s$bin[1:split.at], target])
+	split.at <- paste0("c(", paste(paste0("'", tbl.s$bin[1:split.at], "'"), collapse = ", "), ")")
+	res <- data.frame(rf = paste0("`", rf.l, "`"), ssv = min(ssv), split = split.at, y.l = y.l, y.r = y.r)
+return(res)
+}
+#gini
+gini.num <- function(y, x, sp) {
+	ct <- table(y, x < sp)
+	nx <-  apply(ct, 2, sum)
+	n <- sum(ct)
+	pxy <- ct / matrix(rep(nx, each = 2), nrow = 2)
+	omega <- matrix(rep(nx, each = 2), nrow = 2) / n
+	res <- -sum(omega * pxy * (1 - pxy))
+return(res)
+}
+gini.cat <- function(y, x, sp) {
+	ct <- table(y, x%in%sp)
+	nx <-  apply(ct, 2, sum)
+	n <- sum(ct)
+	pxy <- ct / matrix(rep(nx, each = 2), nrow = 2)
+	omega <- matrix(rep(nx, each = 2), nrow = 2) / n
+	res <- -sum(omega * pxy * (1 - pxy))
+return(res)
+}
+#interactions
+extract.interactions <- function(db, tree.info) {
+	if	(nrow(tree.info) == 1) {
+		return(data.frame())
+		}
+	rf.tree <- unique(tree.info$rf[!is.na(tree.info$rf)])
+	tree.leaves <- tree.info[tree.info$terminal%in%"leaf", ]
+	inter.c <- rep(NA, nrow(db))
+	for	(i in 1:nrow(tree.leaves)) {
+		filter <- tree.leaves$filter[i]
+		rf.s <- tree.leaves$rf[i]
+		filter.eval <- paste0(c(paste0("!is.na(", rf.s, ")"), filter), collapse = " & ")
+		indx <- with(db, {eval(parse(text = filter.eval))})
+		inter.c[indx] <- i
+		}
+	interaction <- data.frame(rf.inter = inter.c)
+return(interaction)
+}
+
+
