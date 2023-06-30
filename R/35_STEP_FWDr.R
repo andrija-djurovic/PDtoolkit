82aeeff80b4b3d69a44c6d2957b81319addc21bc
Andrija Djurovic <djandrija@gmail.com>
2023-06-30 12:21:26 +0200

    Update 35_STEP_FWDr.R

--- R/35_STEP_FWDr.R
+++ R/35_STEP_FWDr.R
@@ -111,7 +111,7 @@ stepFWDr <- function(start.model, p.value = 0.05, db, check.start.model = TRUE, 
 	rf.c <- rf.a[!rf.a%in%names(rf.n)[rf.n]]
 	rf.cl <- length(rf.c)
 	rf.cat.o <- vector("list", rf.cl) 
-	if	(length(rf.c) > 0) {
+	if	(rf.cl > 0) {
 		for	(i in 1:rf.cl) {
 			rf.c.l <- rf.c[i]
 	 		cat.o <- summary.tbl(tbl = db, x = rf.c.l, y = target)
